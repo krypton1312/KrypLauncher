6d12563f131a94d03e01cb9f2233ae95d12d94af
kr3pt0n <egor20042013@gmail.com>
2024-05-13 13:06:51 +0200

    Добавьте файлы проекта.

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace KrypLauncher
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AuthLabel = new Label();
+            LoginTextBox = new TextBox();
+            PasswordTextBox = new TextBox();
+            GeneragePasswordLabel = new LinkLabel();
+            LoginButton = new Button();
+            SuspendLayout();
+            // 
+            // AuthLabel
+            // 
+            AuthLabel.AutoSize = true;
+            AuthLabel.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            AuthLabel.Location = new Point(117, 9);
+            AuthLabel.Name = "AuthLabel";
+            AuthLabel.Size = new Size(161, 32);
+            AuthLabel.TabIndex = 0;
+            AuthLabel.Text = "Авторизация";
+            // 
+            // LoginTextBox
+            // 
+            LoginTextBox.CausesValidation = false;
+            LoginTextBox.Location = new Point(105, 60);
+            LoginTextBox.Name = "LoginTextBox";
+            LoginTextBox.PlaceholderText = "example@example.com";
+            LoginTextBox.Size = new Size(186, 23);
+            LoginTextBox.TabIndex = 1;
+            LoginTextBox.TabStop = false;
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.Location = new Point(105, 89);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.PlaceholderText = "Пароль";
+            PasswordTextBox.Size = new Size(186, 23);
+            PasswordTextBox.TabIndex = 2;
+            PasswordTextBox.TabStop = false;
+            // 
+            // GeneragePasswordLabel
+            // 
+            GeneragePasswordLabel.AutoSize = true;
+            GeneragePasswordLabel.Location = new Point(129, 115);
+            GeneragePasswordLabel.Name = "GeneragePasswordLabel";
+            GeneragePasswordLabel.Size = new Size(133, 15);
+            GeneragePasswordLabel.TabIndex = 3;
+            GeneragePasswordLabel.TabStop = true;
+            GeneragePasswordLabel.Text = "Сгенерировать пароль";
+            // 
+            // LoginButton
+            // 
+            LoginButton.Location = new Point(117, 145);
+            LoginButton.Name = "LoginButton";
+            LoginButton.Size = new Size(161, 32);
+            LoginButton.TabIndex = 4;
+            LoginButton.Text = "Войти";
+            LoginButton.UseVisualStyleBackColor = true;
+            LoginButton.Click += LoginButton_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 211);
+            Controls.Add(LoginButton);
+            Controls.Add(GeneragePasswordLabel);
+            Controls.Add(PasswordTextBox);
+            Controls.Add(LoginTextBox);
+            Controls.Add(AuthLabel);
+            Name = "LoginForm";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label AuthLabel;
+        private TextBox LoginTextBox;
+        private TextBox PasswordTextBox;
+        private LinkLabel GeneragePasswordLabel;
+        private Button LoginButton;
+    }
+}
