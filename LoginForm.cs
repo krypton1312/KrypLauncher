4bb4d3d0b89d62769f2b338906f954d90cfbf028
kr3pt0n <egor20042013@gmail.com>
2024-05-13 16:22:20 +0200

    login

--- LoginForm.cs
+++ LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,31 @@ namespace KrypLauncher
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            String loginUser = LoginTextBox.Text;
+            String PassUser = PasswordTextBox.Text;
 
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @uL AND `password` = @pL", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@pL", MySqlDbType.VarChar).Value = PassUser;
+
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count > 0 )
+            {
+                MessageBox.Show("yea baby");
+            }
+            else
+            {
+                MessageBox.Show("no");
+            }
         }
     }
 }
